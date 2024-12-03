c869b791a0aa055cf74fc82aebeae3ebd7d18200
sam2776 <smikheeva27@gmail.com>
2024-12-03 18:15:44 +0400

    first

--- 1.py
+++ 1.py
@@ -6,7 +6,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('Again')
-    print('UFO came and added this line')
+    print('YandexLyceum')
     my_superfunction()
 
 
